5b0e35814d894cede3e591eaa62359be8c35af43
Seung-hwan285 <ghks285@naver.com>
2022-01-18 09:54:00 +0900

    refactor : carTemplate.js 불필요한 console.log 삭제 , if~else 삭제

--- js/src/carTemplate.js
+++ js/src/carTemplate.js
@@ -4,24 +4,37 @@ import {PATTERN, PATTERN_SPC} from "../utils/constant.js";
 const $=(s)=>document.querySelector(s);
 
 
-export const carTemplate =(carName)=>{
 
-    carName=carName.trim();
-    console.log(carName.length);
+const carNameLength =(carName)=>{
     if(carName.length > 5){
         alert('5자 이하만 입력가능');
     }
-    else if(carName.match(PATTERN)){
+};
+
+const carNameisDuplicate =(carName)=>{
+    if(carName.match(PATTERN)) {
         alert('공백이 존재합니다');
     }
-    else if(carName.match(PATTERN_SPC)){
+};
+
+const caramePatternSpc =(carName)=>{
+    if(carName.match(PATTERN_SPC)){
         alert('특수문자가 존재합니다');
     }
-    else{
+}
+
+const carTemplate =(carName)=>{
+
+    carName=carName.trim();
+
+    carNameLength(carName);
+    carNameisDuplicate(carName);
+    caramePatternSpc(carName);
+    
         return `<div>
             <div class="car-player mr-2">${carName}</div>
         </div>`;
-    }
+
 
 };
 
